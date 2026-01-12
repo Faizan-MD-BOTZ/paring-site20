7b38795af95de168f8f0855010b5977960c8a179
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2026-01-12 21:43:11 +0500

    Update qr.js

--- qr.js
+++ qr.js
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function Arslan_MD_QR_CODE() {
+	async function FAIZAN-MD⁸⁷³_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Arslan_Tech = Arslan_Tech({
+			let Qr_Code_By_FAIZAN-MD⁸⁷³ = Arslan_Tech({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Arslan_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Arslan_Tech.ev.on("connection.update", async (s) => {
+			Qr_Code_By_FAIZAN-MD⁸⁷³.ev.on('creds.update', saveCreds)
+			Qr_Code_By_FAIZAN-MD⁸⁷³.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -56,46 +56,46 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Arslan_Tech.sendMessage(Qr_Code_By_Arslan_Tech.user.id, { text: 'ARSLAN-MD~' + b64data });
+				   let session = await Qr_Code_By_FAIZAN-MD⁸⁷³.sendMessage(Qr_Code_By_FAIZAN-MD⁸⁷³.user.id, { text: 'FAIZAN-MD~' + b64data });
 	
-				   let Arslan_MD_TEXT = `
+				   let FAIZAN-MD⁸⁷³_TEXT = `
 ╔════════════════════◇
 ║『 SESSION CONNECTED』
-║ ✨Arslan-MD🔷
-║ ✨ArslanMD OFFICIAL🔷
+║ ✨FAIZAN-MD⁸⁷³🔷
+║ ✨FAIZAN-MD⁸⁷³ OFFICIAL🔷
 ╚════════════════════╝
 
 
 ---
 
 ╔════════════════════◇
-║『 YOU'VE CHOSEN Arslan-MD 』
+║『 YOU'VE CHOSEN FAIZAN-MD⁸⁷³ 』
 ║ -Set the session ID in Heroku:
 ║ - SESSION_ID: 
 ╚════════════════════╝
 ╔════════════════════◇
 ║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
 ║❍ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: youtube.com/@arslanmdofficial
-║❍ 𝐎𝐰𝐧𝐞𝐫: 923237045919
-║❍ 𝐑𝐞𝐩𝐨: https://github.com/Arslan-MD/Arslan_MD 
-║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/KRyARlvcUjoIv1CPSSyQA5?mode=wwt
-║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306
+║❍ 𝐎𝐰𝐧𝐞𝐫: 923266105873
+║❍ 𝐑𝐞𝐩𝐨: https://github.com/Faizan-MD-BOTZ/Faizan-Ai 
+║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://whatsapp.com/channel/0029Vb6oi4A1iUxR1nPxcF1P
+║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029Vb6oi4A1iUxR1nPxcF1P
 ║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/arslanmd
 ║ ☬ ☬ ☬ ☬
 ╚═════════════════════╝
-𒂀 Enjoy Arslan-MD
+𒂀 Enjoy FAIZAN-MD⁸⁷³
 
 
 ---
 
 Don't Forget To Give Star⭐ To My Repo
 ______________________________`;
-	 await Qr_Code_By_Arslan_Tech.sendMessage(Qr_Code_By_Arslan_Tech.user.id,{text:Arslan_MD_TEXT},{quoted:session})
+	 await Qr_Code_By_FAIZAN-MD.sendMessage(Qr_Code_By_FAIZAN-MD⁸⁷³.user.id,{text:FAIZAN-MD⁸⁷³_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Arslan_Tech.ws.close();
+					await Qr_Code_By_FAIZAN-MD⁸⁷³.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
@@ -112,6 +112,6 @@ ______________________________`;
 			await removeFile("temp/" + id);
 		}
 	}
-	return await Arslan_MD_QR_CODE()
+	return await FAIZAN-MD⁸⁷³_QR_CODE()
 });
 module.exports = router
